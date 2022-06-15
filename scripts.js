80f3b0960a26f62913b87d1fe3d209b685559c08
ludobegins <ludo.beghin@gmail.com>
2022-06-15 10:39:49 -0300

    validacao numero cartas start

--- scripts.js
+++ scripts.js
@@ -1,9 +1,17 @@
-let n_cartas = prompt('Com quantas cartas quer jogar? (de 4 a 14)');
+let n_cartas = 1;
+
+while (((n_cartas % 2 == 0) && (typeof(n_cartas) == 'number') && (n_cartas >= 4) && (n_cartas <= 14)) == false){
+    n_cartas = prompt('Com quantas cartas quer jogar? (números pares de 4 a 14)');
+    n_cartas = Number(n_cartas);
+}
 
 const cartas = [];
 let i = 0;
 
-card_html = "<div class='card'></div>";
+card_html =
+`<div class='card'>
+<img src='img/back_face.png'>
+</div>`;
 
 cards_container = document.querySelector(".cards-container")
 
