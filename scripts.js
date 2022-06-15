fd6576899f325ecc376a39d1a1d94ad565c80958
ludobegins <ludo.beghin@gmail.com>
2022-06-15 10:58:47 -0300

    preparacao distribuicao aleatoria

--- scripts.js
+++ scripts.js
@@ -5,8 +5,15 @@ while (((n_cartas % 2 == 0) && (typeof(n_cartas) == 'number') && (n_cartas >= 4)
     n_cartas = Number(n_cartas);
 }
 
-const cartas = [];
-let i = 0;
+let front_cartas = ['bobross', 'explody', 'fiesta', 'metal', 'revertit', 'triplets', 'unicorn'];
+front_cartas.sort(comparador);
+
+// lista ta aleatoria. selecionar n_itens da lista = n_cartas/2
+// criar lista de cartas ex. explody explody fiesta fiesta metal metal
+// embaralhar
+// fazer html/css genérico pra front carta, ideia: position absolute/relative, sepa z index
+// pra ficar por cima do back. ou então fazer um display none no back algo assim
+// card_html vai receber também a div com o gif, que vai estar hidden num primeiro momento
 
 card_html =
 `<div class='card'>
@@ -14,8 +21,13 @@ card_html =
 </div>`;
 
 cards_container = document.querySelector(".cards-container")
+let i = 0;
 
 while (i < n_cartas){
     cards_container.innerHTML += card_html;
     i++;
+}
+
+function comparador() { 
+    return Math.random() - 0.5; 
 }
