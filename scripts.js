c3d656e7aeb17dc9fc1b10f78f2add9614cf2d4b
ludobegins <ludo.beghin@gmail.com>
2022-06-17 16:57:38 -0300

    distrib aleatoria + click turnCard

--- scripts.js
+++ scripts.js
@@ -8,26 +8,39 @@ while (((n_cartas % 2 == 0) && (typeof(n_cartas) == 'number') && (n_cartas >= 4)
 let front_cartas = ['bobross', 'explody', 'fiesta', 'metal', 'revertit', 'triplets', 'unicorn'];
 front_cartas.sort(comparador);
 
-// lista ta aleatoria. selecionar n_itens da lista = n_cartas/2
-// criar lista de cartas ex. explody explody fiesta fiesta metal metal
-// embaralhar
-// fazer html/css genérico pra front carta, ideia: position absolute/relative, sepa z index
-// pra ficar por cima do back. ou então fazer um display none no back algo assim
-// card_html vai receber também a div com o gif, que vai estar hidden num primeiro momento
+const front = [];
 
-card_html =
-`<div class='card'>
-<img src='img/back_face.png'>
-</div>`;
+for (let i=0; i < n_cartas/2; i++){
+    front.push(front_cartas[i]);
+    front.push(front_cartas[i]);
+}
 
-cards_container = document.querySelector(".cards-container")
+front.sort(comparador);
+
+let card_html = [];
+
+for (let i=0; i < front.length; i++){
+    card_html.push(`<div class='card' onclick=turnCard(this)>
+    <img src='img/back_face.png' class='back-img'>
+    <img src='img/${front[i]}parrot.gif' class="front-img hidden">
+    </div>`)
+}
+
+let cards_container = document.querySelector(".cards-container")
 let i = 0;
 
 while (i < n_cartas){
-    cards_container.innerHTML += card_html;
+    cards_container.innerHTML += card_html[i];
     i++;
 }
 
 function comparador() { 
     return Math.random() - 0.5; 
+}
+
+function turnCard(el){
+    let back_img = el.querySelector(".back-img");
+    let front_img = el.querySelector(".front-img");
+    back_img.classList.add("hidden");
+    front_img.classList.remove("hidden");
 }
